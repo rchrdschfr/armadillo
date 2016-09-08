0dbd514252e3716e889357feb5687c20bffdb3bd
Richard Schaefer <rchrdschfr@gmail.com>
2016-09-08 12:09:55 +0800

    Configure app to send Slack notification.

--- server/db/none/controllers/slack.js
+++ server/db/none/controllers/slack.js
@@ -1,12 +1,18 @@
 import { post } from 'axios';
 
-const webhookURL = 'http://www.gametabs.net';
+const webhookURL = process.env.SLACK_WEBHOOK_URL;
+const channel = '#koreanchicken';
+const username = "trialextensionbot";
 
 export function send(req, res, next) {
-  post(webhookURL).then((response) => {
+  post(webhookURL, {
+    channel,
+    username,
+    text: `${req.query.name} has requested a trial extension.\n\nTG user ID: ${req.query.tgUserId}\nEmail: ${req.query.email}`
+  }).then((response) => {
     next();
   }).catch((data) => {
-    console.log("Error sending message to Slack", data);
+    console.log("Error sending message to Slack:", data);
 
     res.status(500).send("Error");
   });
