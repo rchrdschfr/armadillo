48a129088fc364df85272e23dcc6c92ee5dc4028
Richard Schaefer <rchrdschfr@gmail.com>
2016-09-08 09:39:13 +0800

    Small cleanup and refactor of front-end entry point.

--- app/client.jsx
+++ app/client.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
@@ -7,8 +7,7 @@ import createRoutes from 'routes';
 import configureStore from 'store/configureStore';
 import preRenderMiddleware from 'middlewares/preRenderMiddleware';
 
-// Grab the state from a global injected into
-// server-generated HTML
+// Grab the state from a global injected into server-generated HTML
 const initialState = window.__INITIAL_STATE__;
 
 const store = configureStore(initialState, browserHistory);
@@ -17,14 +16,9 @@ const routes = createRoutes(store);
 
 /**
  * Callback function handling frontend route changes.
+ * Prevent duplicate fetches when first loaded.
  */
 function onUpdate() {
-  // Prevent duplicate fetches when first loaded.
-  // Explanation: On server-side render, we already have __INITIAL_STATE__
-  // So when the client side onUpdate kicks in, we do not need to fetch twice.
-  // We set it to null so that every subsequent client-side navigation will
-  // still trigger a fetch data.
-  // Read more: https://github.com/choonkending/react-webpack-node/pull/203#discussion_r60839356
   if (window.__INITIAL_STATE__ !== null) {
     window.__INITIAL_STATE__ = null;
     return;
@@ -35,10 +29,7 @@ function onUpdate() {
   preRenderMiddleware(store.dispatch, components, params);
 }
 
-
-// Router converts <Route> element hierarchy to a route config:
-// Read more https://github.com/rackt/react-router/blob/latest/docs/Glossary.md#routeconfig
-render(
+ReactDOM.render(
   <Provider store={store}>
     <Router history={history} onUpdate={onUpdate}>
       {routes}
